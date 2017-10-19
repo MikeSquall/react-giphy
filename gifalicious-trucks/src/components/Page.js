b1d325a24e3210dcc97b2d256c3b469ba9b7084b
mikearbib <arbib.michael@gmail.com>
2017-10-19 22:05:22 +0200

    init app + add bootstrap cdn

---
+++ gifalicious-trucks/src/components/Page.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Page.css';
+
+class Page extends Component {
+  render() {
+    return (
+      <div className="">
+        
+      </div>
+    );
+  }
+}
+
+export default Page;
